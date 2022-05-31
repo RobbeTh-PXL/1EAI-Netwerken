ebc82c00ba6812c1566ccbeb2afd4885e41e0be7
RobbeTh-PXL <12102842@student.pxl.be>
2022-05-31 21:10:47 +0200

    Added start code

---
+++ TCP_Chat/Client/TCP_ChatClient_V2.c
@@ -0,0 +1,114 @@
+#define _WIN32_WINNT 0x0601
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+void print_ip_address( struct addrinfo * ip )
+{
+	void * ip_address;
+	char * ip_version;
+	char ip_string[INET6_ADDRSTRLEN];
+
+	if( ip->ai_family == AF_INET )
+	{ // IPv4
+		struct sockaddr_in *ipv4 = (struct sockaddr_in *)ip->ai_addr;
+		ip_address = &(ipv4->sin_addr);
+		ip_version = "IPv4";
+	}
+	else
+	{ // IPv6
+		struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)ip->ai_addr;
+		ip_address = &(ipv6->sin6_addr);
+		ip_version = "IPv6";
+	}
+
+	inet_ntop( ip->ai_family, ip_address, ip_string, sizeof ip_string );
+	printf( "%s -> %s\n", ip_version, ip_string );
+}
+
+int main( int argc, char * argv[] )
+{
+	WSADATA wsaData; //WSAData wsaData; //Could be different case
+	if( WSAStartup( MAKEWORD(2,0), &wsaData ) != 0 ) // MAKEWORD(1,1) for Winsock 1.1, MAKEWORD(2,0) for Winsock 2.0:
+	{
+		fprintf( stderr, "WSAStartup failed.\n" );
+		exit( 1 );
+	}
+
+	struct addrinfo internet_address_setup, *result_head, *result_item;
+	memset( &internet_address_setup, 0, sizeof internet_address_setup );
+	internet_address_setup.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+	internet_address_setup.ai_socktype = SOCK_STREAM;
+
+	int getaddrinfo_return;
+	getaddrinfo_return = getaddrinfo( "localhost.pxl-ea-ict.be", "24042", &internet_address_setup, &result_head );
+	if( getaddrinfo_return != 0 )
+	{
+		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
+		exit( 2 );
+	}
+
+	struct sockaddr * internet_address;
+	size_t internet_address_length;
+	int internet_socket;
+
+	result_item = result_head; //take first of the linked list
+	while( result_item != NULL ) //while the pointer is valid
+	{
+		internet_socket = socket( result_item->ai_family, result_item->ai_socktype, result_item->ai_protocol );
+		if( internet_socket == -1 )
+		{
+			perror( "socket" );
+		}
+		else
+		{
+			printf( "Connecting to " );
+			print_ip_address( result_item );
+
+			int connect_return;
+			connect_return = connect( internet_socket, result_item->ai_addr, result_item->ai_addrlen );
+			if( connect_return == -1 )
+			{
+				perror( "connect" );
+				close( internet_socket );
+			}
+			else
+			{
+				printf( "Connected\n" );
+				break; //stop running through the linked list
+			}
+		}
+		result_item = result_item->ai_next; //take next in the linked list
+	}
+	if( result_item == NULL )
+	{
+		fprintf( stderr, "socket: no valid socket address found\n" );
+		exit( 4 );
+	}
+	freeaddrinfo( result_head ); //free the linked list
+
+	int number_of_bytes_send = 0;
+	number_of_bytes_send = send( internet_socket, "Hello TCP world!", 16, 0 );
+	if( number_of_bytes_send == -1 )
+	{
+		printf( "errno = %d\n", WSAGetLastError() ); //https://docs.microsoft.com/en-us/windows/win32/winsock/windows-sockets-error-codes-2
+		perror( "send" );
+	}
+
+	int shutdown_return;
+	shutdown_return = shutdown( internet_socket, SD_SEND ); //Shutdown Send == SD_SEND ; Receive == SD_RECEIVE ; Send/Receive == SD_BOTH ; https://blog.netherlabs.nl/articles/2009/01/18/the-ultimate-so_linger-page-or-why-is-my-tcp-not-reliable --> Linux : Shutdown Send == SHUT_WR ; Receive == SHUT_RD ; Send/Receive == SHUT_RDWR
+	if( shutdown_return == -1 )
+	{
+		printf( "errno = %d\n", WSAGetLastError() ); //https://docs.microsoft.com/en-us/windows/win32/winsock/windows-sockets-error-codes-2
+		perror( "shutdown" );
+	}
+
+	close( internet_socket );
+
+	WSACleanup();
+
+	return 0;
+}
